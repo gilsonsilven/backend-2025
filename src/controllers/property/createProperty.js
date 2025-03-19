f33839ae5de2cc210a6fc84b1d00e6bfd0b99f71
gilsonsilven <gilsonsilven@gmail.com>
2025-03-18 22:05:24 -0300

    feat: create property model & controllers

--- src/controllers/property/createProperty.js
+++ src/controllers/property/createProperty.js
@@ -1,11 +1,12 @@
-export default function createProperty(req, res) {
+import { create } from "../../models/propertyModel.js"
+
+export default async function createProperty(req, res) {
+    const property = req.body
+
+    const result = await create(property)
+
     return res.json({
         message: "Imóvel criado com sucesso",
-        property: {
-          id: 1,
-          areaTotal: "300m²",
-          endereco: "Rua dos testes",
-          numeroDeQuartos: "3"     
-        }
+        property: result
     })
 }
